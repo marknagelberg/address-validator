d4168e9b1ad5db289ebaf7fa375c101fb67fc691
macbook <nagelbergmark@gmail.com>
2016-04-24 11:19:25 -0500

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='address_validator',
+      version='1.0',
+      description='Utility to validate addresses using Google Geocode API',
+      author='Mark Nagelberg',
+      author_email='nagelbergmark@gmail.com',
+      packages=['address_validator'],
+      )
